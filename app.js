7b57a1e1084292c3a447229589e6b66a9e96de84
Vincent1976 <jiangxinmin@163.com>
2021-11-25 11:24:24 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,43 @@
+// app.js
+App({
+  onLaunch() {
+    // 展示本地存储能力
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId 
+        if (res.code) {
+          wx.request({
+            url: 'http://www.taijuai.com/route/wechat/Get_WXApp_OPENID',
+            //仅为示例，并非真实的接口地址
+            method: "GET",
+            data: {
+              "code": res.code
+            },
+            header: {
+              'content-type': 'application/json'
+              // 默认值
+            },
+            success: function (res) {
+              if (res.data.openid) {
+                wx.setStorage({
+                  key: "tokenId",
+                  data: res.data.openid,
+                })
+              }
+            }
+          })
+        }
+      }
+
+    })
+
+  },
+  globalData: {
+    userInfo: null
+  }
+})
